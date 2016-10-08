c9bb0318db5acf3f305720da75b096819be7fe39
Benjamin M. Brown <benjaminbb@gmail.com>
2016-10-08 04:26:39 -0500

    setting up boilerplate stuff

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'cheap-module-eval-source-map',
+  // Entry, which tells Webpack where to start building our project’s dependency tree;
   entry: [
     // necessary for hot reloading with IE:
     'eventsource-polyfill',
@@ -12,20 +13,27 @@ module.exports = {
     // your code:
     './src/index'
   ],
+  // Output, which tells Webpack where to put the result. This is what our index.html file loads;
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
+  /// Plugins, which tells Webpack which plugins to use when building our code;
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
+    //Loaders, which tells Webpack about the different file loaders we’d like to use.
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.less$/,
+      loader: 'style!css!less'
     }]
   }
 };
