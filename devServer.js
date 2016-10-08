79fc76cc087fc45fd1d9a5b47866879bd3641d9a
Benjamin M. Brown <benjaminbb@gmail.com>
2016-10-08 05:10:20 -0500

    update server

--- devServer.js
+++ devServer.js
@@ -20,7 +20,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(5000, function(err) {
+app.listen(process.env.PORT || 5000, function(err) {
   if (err) {
     console.log(err);
     return;
